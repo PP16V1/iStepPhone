811213c31ab00ccf83aed5a96765d24374e2dbd4
Darcwol <kirill.maneychik@gmail.com>
2017-07-14 22:25:38 +0300

    Add SMS
    Add class Sender & Recipient
    Add class SMS
    Add method to send EMail

---
+++ code/iStepPhone/SMS/Recipient.cs
@@ -0,0 +1,11 @@
+﻿namespace iStepPhone.SMS
+{
+    public class Recipient
+    {
+        public string Adress { get; set; }
+        public Recipient(string adress)
+        {
+            Adress = adress;
+        }
+    }
+}
---
+++ code/iStepPhone/SMS/SMS.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace iStepPhone.SMS
+{
+    public class SMS
+    {
+        Sender sender;
+        Recipient recipient;
+
+        public string Send()
+        {
+            MailAddress From = new MailAddress(sender.Adress);
+            MailAddress To = new MailAddress(recipient.Adress);
+            Console.Write("Please write the body: ");
+            MailMessage message = new MailMessage(From, To)
+            {
+                Body = Console.ReadLine()
+            };
+            SmtpClient client = new SmtpClient("smtp.gmail.com", 587)
+            {
+                Credentials = new NetworkCredential(sender.Adress, sender.Password),
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                EnableSsl = true
+            };
+            try
+            {
+                client.Send(message);
+                return "EMail was send succesfully";
+            }
+            catch (Exception e)
+            {
+                return "There is a mistake: " + e.Message;
+            }
+        }
+        public string Send(string body)
+        {
+            MailAddress From = new MailAddress(sender.Adress);
+            MailAddress To = new MailAddress(recipient.Adress);
+            Console.Write("Please write the body: ");
+            MailMessage message = new MailMessage(From, To)
+            {
+                Body = body
+            };
+            SmtpClient client = new SmtpClient("smtp.gmail.com", 587)
+            {
+                Credentials = new NetworkCredential(sender.Adress, sender.Password),
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                EnableSsl = true
+            };
+            try
+            {
+                client.Send(message);
+                return "EMail was send succesfully";
+            }
+            catch (Exception e)
+            {
+                return "There is a mistake: " + e.Message;
+            }
+        }
+        public SMS(string SenderAdress, string SenderPassword, string RecipientAdress)
+        {
+            sender = new Sender(SenderAdress, SenderPassword);
+            recipient = new Recipient(RecipientAdress);
+        }
+    }
+
+}
