bc95390fc1b0eac2f2556c0d3187b2375e94e475
IevgenSuturin <esuturin77@gmail.com>
2017-07-15 23:08:24 +0300

    File manager
    Explorer was developed.

---
+++ code/iStepPhone/FileManager/Abstract/AUIExplorer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using iStepPhone.FileManager.Factory;
+
+namespace iStepPhone.FileManager.Abstract
+{
+    public abstract class AUIExplorer
+    {
+        public abstract void ShowExplorer(List<UserDirectoryClass> Items);
+    }
+}
