4ee47448b01bc65e822e2d155d0a35911ca8cb9b
Darcwol <kirill.maneychik@gmail.com>
2017-07-21 00:31:32 +0300

    Add Test for SMS

--- code/iStepPhone/SMS/SMS.cs
+++ code/iStepPhone/SMS/SMS.cs
@@ -10,12 +10,12 @@ using System.Threading.Tasks;
 
 namespace iStepPhone.SMS
 {
-    public class SMS
+    public class Sms
     {
         Sender sender;
         Recipient recipient;
         /// <summary>
-        /// Send SMS via gmail.com. You shoud write down body in console.
+        /// Send Sms via gmail.com. You shoud write down body in console.
         /// </summary>
         /// <returns>Work report</returns>
         public string Send()
@@ -45,7 +45,7 @@ namespace iStepPhone.SMS
             
         }
         /// <summary>
-        /// Send SMS via gmail.com. 
+        /// Send Sms via gmail.com. 
         /// </summary>
         /// <param name="body">Main text</param>
         /// <returns>Work report</returns>
@@ -79,10 +79,33 @@ namespace iStepPhone.SMS
         /// <param name="SenderAdress">Sender adress. Example: sender@gmail.com</param>
         /// <param name="SenderPassword">Sender password. Example: 12345678</param>
         /// <param name="RecipientAdress">Recipient adress. Example: recipient@gmail.com</param>
-        public SMS(string SenderAdress, string SenderPassword, string RecipientAdress)
+        public Sms(string SenderAdress, string SenderPassword, string RecipientAdress)
         {
-            sender = new Sender(SenderAdress, SenderPassword);
-            recipient = new Recipient(RecipientAdress);
+            this.sender = new Sender(SenderAdress, SenderPassword);
+            this.recipient = new Recipient(RecipientAdress);
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="recipient">Recipient class instance</param>
+        /// <param name="sender">Sender class instance</param>
+        public Sms(Recipient recipient, Sender sender)
+        {
+            this.sender = sender;
+            this.recipient = recipient;
+        }
+        /// <summary>
+        /// Using this constructor user shoud write all data manually
+        /// </summary>
+        public Sms()
+        {
+            sender = new Sender();
+            Console.Write("Enter sender adress: ");
+            sender.Adress = Console.ReadLine();
+            Console.Write("Enter sender password: ");
+            sender.Password = Console.ReadLine();
+            Console.Write("Enter recipient adress: ");
+            recipient = new Recipient(Console.ReadLine());
         }
     }
 
